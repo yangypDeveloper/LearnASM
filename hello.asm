93820c95160ffda46c056a83ce3f5969f5413f63
Yoli <yangyp@fableedu.com>
2018-03-21 11:18:46 +0800

    note
    增加部分注释

--- hello.asm
+++ hello.asm
@@ -13,12 +13,12 @@ kernel:
     ret
 
 _main:
-    mov rax,0x2000004
-    mov rdi,1
-    mov rsi,msg
-    mov rdx,len
+    mov rax,0x2000004   ;0x2000004 表示 syscall 调用号 write
+    mov rdi,1           ;表示控制台输出
+    mov rsi,msg         ;syscall 调用会到 rsi 来获取字符
+    mov rdx,len         ;字符串长度
     call kernel
 
-    mov rax,0x2000001
+    mov rax,0x2000001   ;0x2000001 表示退出 syscall  
     mov rdi,0
     call kernel
